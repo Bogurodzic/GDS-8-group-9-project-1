3a497dcab0a8112621757990c5dd3770e589e2b9
Kamil Jarząb <jarzbkamil@gmail.com>
2021-01-10 14:42:00 +0100

    Changed proectiles assets, position, speed. Removed olds assets.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -282,12 +282,12 @@ public class PlayerController : MonoBehaviour
 
     private void CreateVerticalProjectile()
     {
-        Instantiate(verticalProjectile, transform.position, verticalProjectile.transform.rotation);
+        Instantiate(verticalProjectile, new Vector3(transform.position.x - 0.5f, transform.position.y + 0.2f, transform.position.z), verticalProjectile.transform.rotation);
     }
 
     private void CreateHorizontalProjectile()
     {
-        Instantiate(horizontalProjectile, transform.position, horizontalProjectile.transform.rotation);
+        Instantiate(horizontalProjectile, new Vector3(transform.position.x + 1.1f, transform.position.y - 0.23f, transform.position.z), horizontalProjectile.transform.rotation);
     }
 
     private bool ShouldPlayerAccelerate(bool ignoreKey = false)
