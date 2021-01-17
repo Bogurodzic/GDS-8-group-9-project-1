bf2e83e200b1a27b7bcfbcd9231b2469403fa4bf
Kamil Jarząb <jarzbkamil@gmail.com>
2021-01-17 19:06:57 +0100

    Added bombs and explosions

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
+using DragonBones;
 using UnityEngine;
 using Random = UnityEngine.Random;
 using Vector2 = UnityEngine.Vector2;
@@ -10,9 +11,9 @@ public class Bomb : MonoBehaviour
 {
 
     public float bombFireForce;
-
     private Rigidbody2D _bombRigidBody;
-
+    private UnityArmatureComponent _bombAnimation;
+    private bool _destroyInitialised = false;
     public enum BombDirection
     {
         Left,
@@ -21,30 +22,48 @@ public class Bomb : MonoBehaviour
 
     void Start()
     {
-        
+        LoadComponents();
     }
 
     public void StartObject(BombDirection bombDirection)
     {
-        GameManager.Instance.BombDeployed();
         LoadComponents();
+        _bombAnimation.animation.Play("bomb_idle", 2);
+        GameManager.Instance.BombDeployed();
         _bombRigidBody.AddForce(new Vector2(GetBombDirection(bombDirection)  * (bombFireForce / 2), 1 * bombFireForce), ForceMode2D.Impulse);
+
+
     }
 
     void Update()
     {
-        
+        if (_bombAnimation.animation.isCompleted && !_destroyInitialised)
+        {
+            _bombAnimation.animation.Play("bomb_fly", 1);
+        } else if (_destroyInitialised)
+        {
+            if (_bombAnimation.animation.isCompleted)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 
     private void LoadComponents()
     {
         _bombRigidBody = GetComponent<Rigidbody2D>();
+        _bombAnimation = GetComponent<UnityArmatureComponent>();
     }
     
     private void DestroyBomb()
     {
         GameManager.Instance.BombDestroyed();
-        Destroy(gameObject);
+
+        _destroyInitialised = true;
+        _bombAnimation.animation.Play("bomb_explosion", 1);
+       // _boxCollider2D.enabled = false;
+        _bombRigidBody.Sleep();
+        
     }
     
     void OnTriggerEnter2D(Collider2D collision)
