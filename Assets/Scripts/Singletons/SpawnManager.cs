f894d830bc03ece67b5c82e434befb5072292ce2
Kamil Jarząb <jarzbkamil@gmail.com>
2021-01-18 00:00:39 +0100

    Prototype of spawnmanager

---
+++ Assets/Scripts/Singletons/SpawnManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : GenericSingletonClass<SpawnManager>
+{
+    public SpawnPoint[] SpawnPoint;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
+
+
+[Serializable]
+public class SpawnPoint
+{
+    public float progressRequired;
+    public GameObject enemyPrefabToSpawn;
+    public SpawnPointIntensity spawnPointIntensity;
+}
+
+[Serializable]
+public class SpawnPointIntensity
+{
+    public double left;
+    public double topLeft;
+    public double topCenter;
+    public double topRight;
+    public double right;
+}
