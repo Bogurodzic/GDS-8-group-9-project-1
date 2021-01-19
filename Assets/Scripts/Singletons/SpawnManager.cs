70e9beec5b801463d45772e145e26a0bc1ade89d
Kamil Jarząb <jarzbkamil@gmail.com>
2021-01-19 20:54:53 +0100

    Spawn Manager now properly spawns enemies.

--- Assets/Scripts/Singletons/SpawnManager.cs
+++ Assets/Scripts/Singletons/SpawnManager.cs
@@ -5,7 +5,15 @@ using UnityEngine;
 
 public class SpawnManager : GenericSingletonClass<SpawnManager>
 {
-    public SpawnPoint[] SpawnPoint;
+    public SpawnPoint[] spawnPoints;
+    private Progress _progress;
+
+
+    public GameObject leftSpawn;
+    public GameObject leftTopSpawn;
+    public GameObject centerTopSpawn;
+    public GameObject rightTopSpawn;
+    public GameObject rightSpawn;
     void Start()
     {
         
@@ -13,9 +21,59 @@ public class SpawnManager : GenericSingletonClass<SpawnManager>
 
     void Update()
     {
-        
+        float progress = GameManager.Instance.GetProgress() * 100;
+
+        foreach (var spawnPoint in spawnPoints)
+        {
+            if (progress >= spawnPoint.progressRequired && !spawnPoint.spawned)
+            {
+                ExecuteSpawnPoint(spawnPoint);
+            }
+        }
     }
+
+
+    private void ExecuteSpawnPoint(SpawnPoint spawnPoint)
+    {
+        spawnPoint.spawned = true;
+
+        int enemiesQuantity = spawnPoint.spawnPointIntensity.left + spawnPoint.spawnPointIntensity.topLeft +
+                              spawnPoint.spawnPointIntensity.topCenter + spawnPoint.spawnPointIntensity.topRight + spawnPoint.spawnPointIntensity.right;
+        
+        if (spawnPoint.spawnPointIntensity.left > 0)
+        {
+            SpawnEnemy(spawnPoint.enemyPrefabToSpawn, leftSpawn, enemiesQuantity);
+        }
+        if (spawnPoint.spawnPointIntensity.topLeft > 0)
+        {
+            SpawnEnemy(spawnPoint.enemyPrefabToSpawn, leftTopSpawn, enemiesQuantity);
+        }
+        if (spawnPoint.spawnPointIntensity.topCenter > 0)
+        {
+            SpawnEnemy(spawnPoint.enemyPrefabToSpawn, centerTopSpawn, enemiesQuantity);
+        }
+        if (spawnPoint.spawnPointIntensity.topRight > 0)
+        {
+            SpawnEnemy(spawnPoint.enemyPrefabToSpawn, rightTopSpawn, enemiesQuantity);
+        }
+        if (spawnPoint.spawnPointIntensity.right > 0)
+        {
+            SpawnEnemy(spawnPoint.enemyPrefabToSpawn, rightSpawn, enemiesQuantity);
+        }
+        
+        Debug.Log("SPAWNED ENEMY: " + spawnPoint.progressRequired + " ::: quantity: " + enemiesQuantity );
+
+    }
+
+    private void SpawnEnemy(GameObject enemyPrefab, GameObject spawnLocationGameObject, int enemiesQuantity)
+    {
+        Instantiate(enemyPrefab, spawnLocationGameObject.transform.position,
+            spawnLocationGameObject.transform.rotation);
+    }
+
+
 }
+
 
 
 [Serializable]
@@ -24,14 +82,15 @@ public class SpawnPoint
     public float progressRequired;
     public GameObject enemyPrefabToSpawn;
     public SpawnPointIntensity spawnPointIntensity;
+    public bool spawned = false;
 }
 
 [Serializable]
 public class SpawnPointIntensity
 {
-    public double left;
-    public double topLeft;
-    public double topCenter;
-    public double topRight;
-    public double right;
+    public int left;
+    public int topLeft;
+    public int topCenter;
+    public int topRight;
+    public int right;
 }
