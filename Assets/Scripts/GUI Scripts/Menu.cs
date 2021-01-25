a9ed614cf2cd7d147c66d4772341da448041de99
Kamil Jarząb <jarzbkamil@gmail.com>
2021-01-25 20:23:22 +0100

    Added quitting from game

--- Assets/Scripts/GUI Scripts/Menu.cs
+++ Assets/Scripts/GUI Scripts/Menu.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,5 +9,20 @@ public class Menu : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadScene("Scenes/Levels/Level1");
+    }
+
+
+    public void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            QuitGame();
+        }
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
