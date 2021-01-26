abf3478002901902ec400ebad73273ec17a965a2
Kamil Jarząb <jarzbkamil@gmail.com>
2021-01-26 17:21:41 +0100

    Now obstacles dont fall after being destroyed

--- Assets/Scripts/Obstacles/RollingRockBig.cs
+++ Assets/Scripts/Obstacles/RollingRockBig.cs
@@ -25,6 +25,6 @@ public class RollingRockBig : RollingRock
         _destroyInitialised = true;
         _rockAnimation.animation.Play("boulder_destroy_big", 1);
         _boxCollider2D.enabled = false;
-        _rigidbody2D.Sleep();
+        _rigidbody2D.bodyType = RigidbodyType2D.Kinematic;
     }
 }
